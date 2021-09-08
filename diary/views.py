3a686230e72f40ed317d8397be9b4fdac6f45003
OhgamiKanako <o2605.rf8k21d5ptr@gmail.com>
2021-09-08 07:15:57 +0000

    test

--- diary/views.py
+++ diary/views.py
@@ -26,5 +26,5 @@ class InquiryView(generic.FormView):
     def from_valid(self, form):
         form.send_email()
         messages.success(self.request, 'メッセージを送信しました。')
-        # logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        # logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))ここ保留
         return super().form_valid(form)
